ab4ed8d1ad26f7bdbc863163acf0974672cb84fc
Fabiana <fabibarrionuevo1@gmail.com>
2021-05-19 20:07:43 -0300

    add scroll to top button

--- app.js
+++ app.js
@@ -7,3 +7,25 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
   }
+
+
+/*Go to top function */
+
+mybutton = document.getElementById("#myButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {scrollFunction()}
+
+function scrollFunction() {
+  if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+    myButton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function goToTopFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
