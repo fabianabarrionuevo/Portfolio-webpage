ce0c157824cf0fe22afa1769ad9408757ddfaf55
Fabiana <fabibarrionuevo1@gmail.com>
2021-07-29 20:49:33 -0300

    cambios en dispositivos pequeños

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "100%";
+    document.getElementById("mySidepanel").style.width = "90%";
   }
   
   /* Set the width of the sidebar to 0 (hide it) */
